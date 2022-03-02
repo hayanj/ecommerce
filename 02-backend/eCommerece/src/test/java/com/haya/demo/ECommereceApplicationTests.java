f4f00ec4cb07de7f85b88b80a8b117064c767db7
hayanj <hayaaljuraysi@gmail.com>
2022-03-02 13:08:04 +0300

    realse 1

---
+++ 02-backend/eCommerece/src/test/java/com/haya/demo/ECommereceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haya.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommereceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
