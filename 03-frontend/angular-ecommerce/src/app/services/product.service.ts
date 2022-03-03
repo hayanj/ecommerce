072f1d421bf65c0083070dccec9c881c51bfe73b
hayanj <hayaaljuraysi@gmail.com>
2022-03-03 12:42:42 +0300

    Enhance product list

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -11,7 +11,8 @@ export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products'
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    // @TODO: need to build URL based on category id ... will come to this later!
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
     );
